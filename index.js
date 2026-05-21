2731fe76c813c4f0a6d635145fb4baeeb3185f41
Adão Richard <adaorichard146@gmail.com>
2026-05-21 14:59:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://SEU-URL-NETLIFY.netlify.app"
+    "https://devstoque.netlify.app"
   ],
   credentials: true
 }));
